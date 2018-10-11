b4a67a8ab12bfb27256429743a42983b3b741cf4
Khairul <khairulice@gmail.com>
2018-10-11 17:30:39 +0600

    File upload

--- src/app/portfolio/profile/profile.component.ts
+++ src/app/portfolio/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload/ng2-file-upload';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 
-const URL = 'http://localhost:4000/api/';
+import {  FileUploader } from 'ng2-file-upload/ng2-file-upload';
+import { HttpClient } from '@angular/common/http';
+const URL = 'http://localhost:4000/api';
 
 @Component({
   selector: 'app-profile',
@@ -9,12 +10,36 @@ const URL = 'http://localhost:4000/api/';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  public uploader:FileUploader = new FileUploader({url: URL});
-
-  constructor() { }
+  public uploader:FileUploader = new FileUploader({url: URL, itemAlias: 'photo'});
+  title = 'app works!';
 
   ngOnInit() {
-  }
+	 this.uploader.onAfterAddingFile = (file)=> { file.withCredentials = false; };
+	 this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+            console.log("ImageUpload:uploaded:", item, status, response);
+            alert(response);
+        };
+	}
 
+	constructor(private http: HttpClient, private el: ElementRef) {}
+	
+	upload() {
+        let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
+        console.log("iam+ "+inputEl);
+        let fileCount: number = inputEl.files.length;
+        let formData = new FormData();
+        if (fileCount > 0) { // a file was selected
+            for (let i = 0; i < fileCount; i++) {
+                formData.append('photo', inputEl.files.item(i));
+            }
+            this.http
+                .post(URL, formData).pipe((res:any) => res).subscribe(
+                    (success) => {
+                     alert(success);
+                  },
+                    (error) => alert(error)
+                );
+
+        }
+       }
 }
